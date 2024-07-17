be9e4565dddcf4ecd0b3e718860f4038945841a1
Donatas Daubaras <donatasdaubar@gmail.com>
2024-07-17 14:49:17 +0300

    Initial commit

---
+++ src/main/java/com/donatasd/wixhomework/WixHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.donatasd.wixhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WixHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WixHomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/donatasd/wixhomework/WixHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donatasd.wixhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WixHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
